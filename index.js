beec59b43e99a513b391f27c7d14c7c56602e700
GGOPALKRISHNA <gg647262@gmail.com>
2025-05-11 06:17:42 +0000

    compose & health

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ mongoose.connect(process.env.MONGO_URI , {
 .catch((err)=> console.log('error in connection... ', err))
 
 app.use('/sales', saleRoutes)
-
+app.get('/health', async (req, res)=> {
+  try{
+    res.status(200).json({status: "OK"})
+  }catch(err){
+    res.status(500).json({status: "DOWN", error: err.message})
+  }
+})
 
 const port = process.env.PORT;
 app.listen(port,'0.0.0.0' , ()=>{
